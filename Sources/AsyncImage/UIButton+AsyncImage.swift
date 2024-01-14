638668c50d12ffccb8185922952e08a320452b6c
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-01-14 15:38:30 +0900

    Update

--- Sources/AsyncImage/UIButton+AsyncImage.swift
+++ Sources/AsyncImage/UIButton+AsyncImage.swift
@@ -1,26 +1,30 @@
 import UIKit
 
-private let logger = makeLogger()
-
 extension UIButton {
     private static var tasks: [ObjectIdentifier: Task<Void, Never>] = [:]
     
-    public func setImage(from url: URL?, loader: AsyncImageLoader = .shared) {
+    public func setImage(from url: URL?, loader: AsyncImageLoader = .shared, completion: ((Error?) -> Void)? = nil) {
         let id = ObjectIdentifier(self)
         Self.tasks[id]?.cancel()
+        Self.tasks[id] = nil
         setImage(nil, for: .normal)
-        if let url {
-            if let image = loader.cachedImage(from: url) {
-                setImage(image, for: .normal)
-            } else {
-                Self.tasks[id] = Task { [weak self] in
-                    do {
-                        let image = try await loader.image(from: url)
-                        if Task.isCancelled == false {
-                            self?.setImage(image, for: .normal)
-                        }
-                    } catch {
-                        logger.error("\(error)")
+        guard let url else {
+            return
+        }
+        
+        if let image = loader.cachedImage(from: url) {
+            setImage(image, for: .normal)
+        } else {
+            Self.tasks[id] = Task { [weak self] in
+                do {
+                    let image = try await loader.image(from: url)
+                    if !Task.isCancelled {
+                        self?.setImage(image, for: .normal)
+                        completion?(nil)
+                    }
+                } catch {
+                    if !Task.isCancelled {
+                        completion?(error)
                     }
                 }
             }
--- Sources/AsyncImage/UIImageView+AsyncImage.swift
+++ Sources/AsyncImage/UIImageView+AsyncImage.swift
@@ -1,26 +1,30 @@
 import UIKit
 
-private let logger = makeLogger()
-
 extension UIImageView {
     private static var tasks: [ObjectIdentifier: Task<Void, Never>] = [:]
     
-    public func setImage(from url: URL?, loader: AsyncImageLoader = .shared) {
+    public func setImage(from url: URL?, loader: AsyncImageLoader = .shared, completion: ((Error?) -> Void)? = nil) {
         let id = ObjectIdentifier(self)
         Self.tasks[id]?.cancel()
+        Self.tasks[id] = nil
         image = nil
-        if let url {
-            if let image = loader.cachedImage(from: url) {
-                self.image = image
-            } else {
-                Self.tasks[id] = Task { [weak self] in
-                    do {
-                        let image = try await loader.image(from: url)
-                        if Task.isCancelled == false {
-                            self?.image = image
-                        }
-                    } catch {
-                        logger.error("\(error)")
+        guard let url else {
+            return
+        }
+        
+        if let image = loader.cachedImage(from: url) {
+            self.image = image
+        } else {
+            Self.tasks[id] = Task { [weak self] in
+                do {
+                    let image = try await loader.image(from: url)
+                    if !Task.isCancelled {
+                        self?.image = image
+                        completion?(nil)
+                    }
+                } catch {
+                    if !Task.isCancelled {
+                        completion?(error)
                     }
                 }
             }
