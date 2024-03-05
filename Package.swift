bc25c7992e5580043be4cd5c213121039711c297
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-05 18:50:20 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "AsyncImage", targets: ["AsyncImage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/li-bei/swift-file-downloader", from: "0.2.0"),
+        .package(url: "https://github.com/li-bei/swift-file-downloader", from: "0.3.0"),
     ],
     targets: [
         .target(
